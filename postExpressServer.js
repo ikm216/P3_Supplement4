c6b5d1d896b0dc15c857bdfc8ea5cd6afc6f98a6
ikm216 <ikmclassof22@gmail.com>
2024-12-08 19:59:17 -0500

    feat: added feature to takes a JSON body and responds with the content in the body’s “content” field.

--- postExpressServer.js
+++ postExpressServer.js
@@ -1,22 +1,22 @@
-// const express = require('express');
-// const app = express();
+const express = require('express');
+const app = express();
 
-// app.use(express.json());
+app.use(express.json());
 
-// app.post('/', (req, res) => {
-//     const content = req.body.content;
-//     if (content) {
-//         res.send(content);
-//     } else {
-//         res.status(400).send('Content field is required');
-//     }
-// });
+app.post('/', (req, res) => {
+    const content = req.body.content;
+    if (content) {
+        res.send(content);
+    } else {
+        res.status(400).send('Content field is required');
+    }
+});
 
-// if (require.main === module) {
-//     const PORT = 3000;
-//     app.listen(PORT, () => {
-//         console.log(`Server is running on port ${PORT}`);
-//     });
-// }
+if (require.main === module) {
+    const PORT = 3000;
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
 
-// module.exports = app
+module.exports = app
